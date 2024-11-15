089dc7cb9693a4d1c9c4f4485cede2fec859c898
김지후 <80590470+KJH270@users.noreply.github.com>
2024-11-15 23:07:24 +0900

    are u lose?
    nah, I'd win.

--- tester.py
+++ tester.py
@@ -2,13 +2,16 @@ import geo.utils as utils
 import sys
 
 a,b = 3,4
-a = int(sys.argv[1])
-b = int(sys.argv[2])
+if len(sys.argv)>1:
+    a = int(sys.argv[1])
+if len(sys.argv)>2:
+    b = int(sys.argv[2])
 c = utils.pythagoras(a,b)
 print("c = ",c)
 
 r = 10
-r = int(sys.argv[3])
+if len(sys.argv)>3:
+    r = int(sys.argv[3])
 area = utils.circle(r)
 print("area = ",area)
 
