41421ef599a196399fa2cb58ebacb1676e4bde18
김지후 <80590470+KJH270@users.noreply.github.com>
2024-11-15 23:02:49 +0900

    why I used input
    I should use sys

--- tester.py
+++ tester.py
@@ -1,13 +1,14 @@
 import geo.utils as utils
+import sys
 
 a,b = 3,4
-a = int(input())
-b = int(input())
+a = int(sys.argv[1])
+b = int(sys.argv[2])
 c = utils.pythagoras(a,b)
 print("c = ",c)
 
 r = 10
-r = int(input())
+r = int(sys.argv[3])
 area = utils.circle(r)
 print("area = ",area)
 
