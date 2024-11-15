f6af2ee78a38b18b1aac4d35f12dd240eea29008
김지후 <80590470+KJH270@users.noreply.github.com>
2024-11-15 22:55:22 +0900

    okay I forgot input
    omg

--- tester.py
+++ tester.py
@@ -1,10 +1,13 @@
 import geo.utils as utils
 
 a,b = 3,4
+a = int(input())
+b = int(input())
 c = utils.pythagoras(a,b)
 print("c = ",c)
 
 r = 10
+r = int(input())
 area = utils.circle(r)
 print("area = ",area)
 
